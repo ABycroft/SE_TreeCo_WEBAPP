386df82d98eed8eb4ec8da128e827f66d957aa4a
pmc3066-pc <caleb@hines.co.nz>
2019-09-22 19:48:16 +1200

    basic looking store

--- src/data.js
+++ src/data.js
@@ -9,28 +9,28 @@ export const treesData = [
     {
         id: 2,
         name: "Fir",
-        img: "trees/fir_tree.png",
+        img: "trees/fir_tree.jpg",
         price: 1350,
         inCart: false
     },
     {
         id: 3,
         name: "Kauri",
-        img: "trees/kauri_tree.png",
+        img: "trees/kauri_tree.jpg",
         price: 1980,
         inCart: false
     },
     {
         id: 4,
         name: "Oak",
-        img: "trees/oak_tree.png",
+        img: "trees/oak_tree.jpg",
         price: 1250,
         inCart: false
     },
     {
         id: 5,
         name: "Pohutukawa",
-        img: "trees/pohutukawa_tree.png",
+        img: "trees/pohutukawa_tree.jpg",
         price: 1700,
         inCart: false
     }
--- src/store/TreeList.js
+++ src/store/TreeList.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
 import Tree from "./Tree";
-import {treesData} from "../data";
 import {TreesConsumer} from "../context";
 
 // set up bootstrap
 // https://youtu.be/wPQ1-33teR4?t=6727
 
 class TreeList extends Component {
-    state = {
-        trees: treesData
-    }
     render() {
         //console.log(this.state.trees);
         return (
-
-            <div>
-            <div>
+            <div className="container py-5">
                 <h2>Product List</h2>
-                <TreesConsumer>
-                    {value => {
-                        //console.log(value)
-                        return value.products.map( product =>{
-                            return <Tree key={product.id} product={product}/>
-                        })
-                    }}
-                </TreesConsumer>
-            </div>
+                <div className="row">
+                    <TreesConsumer>
+                        {value => {
+                            //console.log(value)
+                            return value.products.map( product =>{
+                                return <Tree key={product.id} product={product}/>
+                            })
+                        }}
+                    </TreesConsumer>
+                </div>
             </div>
         )
     }
