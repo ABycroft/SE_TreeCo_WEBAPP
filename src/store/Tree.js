dc760fdfeda851d119254991f122cd4686fcb976
ABycroft <42327894+ABycroft@users.noreply.github.com>
2019-09-24 15:06:44 +1200

    Change image on click area

--- src/store/Tree.js
+++ src/store/Tree.js
@@ -11,9 +11,9 @@ export default class Tree extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={()=>console.log("image clicked")}>
+                    <div className="img-container p-5" >
                     <Link to="/details">
-                        <img src={img} alt="tree" className="card-img-top mb-1"/>
+                        <img src={img} alt="tree" className="card-img-top mb-1" onClick={()=>console.log("image clicked")}/>
                     </Link>
                     <button className="btnCart" disabled={inCart ? true:false} onClick={()=>{console.log("added to cart clicked")}}>
                         {inCart?(<p className="text-capitalize mb-0" disabled>
