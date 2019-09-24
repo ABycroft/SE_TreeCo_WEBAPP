95c83dc0be3ad511a9e5fd89063b472365cc7ea9
pmc3066 <caleb@hines.co.nz>
2019-09-24 15:59:16 +1200

    tree info

--- src/data.js
+++ src/data.js
@@ -4,34 +4,59 @@ export const treesData = [
         name: "Birch",
         img: "trees/birch_tree.png",
         price: 1200,
-        inCart: false
+        inCart: false,
+        sun: "",
+        category: "",
+        maintenance: "",
+        height: "",
+        soil: ""
     },
     {
         id: 2,
         name: "Fir",
         img: "trees/fir_tree.jpg",
         price: 1350,
-        inCart: false
+        inCart: false,
+        sun: "",
+        category: "",
+        maintenance: "",
+        height: "",
+        soil: ""
     },
     {
         id: 3,
         name: "Kauri",
         img: "trees/kauri_tree.jpg",
         price: 1980,
-        inCart: false
+        inCart: false,
+        sun: "",
+        category: "",
+        maintenance: "",
+        height: "",
+        soil: ""
     },
     {
         id: 4,
         name: "Oak",
         img: "trees/oak_tree.jpg",
         price: 1250,
-        inCart: false
+        inCart: false,
+        sun: "",
+        category: "",
+        maintenance: "",
+        height: "",
+        soil: ""
     },
     {
         id: 5,
         name: "Pohutukawa",
         img: "trees/pohutukawa_tree.jpg",
         price: 1700,
-        inCart: false
+        inCart: false,
+        sun: "",
+        category: "",
+        maintenance: "",
+        height: "",
+        soil: ""
     }
 ]
