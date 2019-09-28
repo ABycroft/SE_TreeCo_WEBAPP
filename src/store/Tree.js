c91df2a7401d1cf5806f0c1fe0d82ea140d86678
pmc3066-pc <caleb@hines.co.nz>
2019-09-28 15:11:01 +1200

    moved Tree.js styling to css and removed semantic ui import

--- src/store/Tree.js
+++ src/store/Tree.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { TreesConsumer } from '../context';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import './tree.css';
 // 1:56:54
 
 export default class Tree extends Component {
@@ -11,20 +12,18 @@ export default class Tree extends Component {
         const {id, name, img, price, inCart} = this.props.product;
         return (
 
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                    <div>
-                    <Card  style={{ width: '18rem',backgroundColor:'#alice',fontFamily:'Montserrat' }}>
+            <ProductWrapper>
+                <Card  className="cardParent">
                     <Card.Img  width="300px" height="200px" variant="top" src={img} alt="tree" className="card-img-top mb-1" onClick={()=>console.log("image clicked")}  />
-                    <Card.Body style={{backgroundColor:'#alice'}}>
-                        <Card.Title style={{color:"#05386b",fontWeight:'bold',fontSize:'20px'}} >{name}</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
-                        <Button  style={{backgroundColor:"#379683",color:"#ffffff"}}  variant="light">Go somewhere</Button>
+                    <Card.Body>
+                        <Card.Title className="cardTitle">{name}</Card.Title>
+                        <p>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </p>
+                        <Button onClick={()=>console.log("button clicked")} style={{backgroundColor:"#379683",color:"#ffffff"}}  variant="light">Go somewhere</Button>
                     </Card.Body>
-                    </Card> 
-                    </div>
+                </Card> 
             </ProductWrapper>
         )
     }
