5de7eba1697a2fe3f17af4765a7fc02e9f887d0f
pmc3066-pc <caleb@hines.co.nz>
2019-10-10 13:56:58 +1300

    adding submit payment validation

--- src/payment/Payment.js
+++ src/payment/Payment.js
@@ -3,7 +3,7 @@ import './payment.css';
 
 export default class Payment extends Component {
     onSubmit = e => {
-        e.preventDefault();
+        
     };
     
     render() {
@@ -13,24 +13,22 @@ export default class Payment extends Component {
                 <br></br>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required/>
+                    <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required/>
                     <small id="emailHelp" className="form-text text-muted">We will never share your email.</small>
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlTextarea1">Name</label>
-                    <input className="form-control" type="text " required />
+                    <input name="name" className="form-control" type="text " required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlTextarea1">Shipping Address</label>
-                    <input className="form-control" type="text" required />
+                    <input name="address" className="form-control" type="text" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlTextarea1">Credit Card Information</label>
-                    <input className="form-control" type="text" required />
+                    <input name="cardinfo" className="form-control" type="text" required />
                 </div>
-                
                 <button type="submit" class="btn purchase" variant="light">Confirm</button>
-                
             </form>
             <br></br>
             </div>
